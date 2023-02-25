a41afda1d0c87c3654bf7501cede602fe0807ae7
suchan <lsuchan0011@naver.com>
2023-02-25 20:11:18 +0900

    data- 숨기기 기능, 이벤트리스너를 한개만 사용해도 됨

--- tab.js
+++ tab.js
@@ -1,11 +1,17 @@
 let tabNum = $('.tab-content').length;
 let tabButton = $('.tab-button');
 let tabContent = $('.tab-content');
-for (let i = 0; i < tabNum; i++) {
-	tabButton.eq(i).on('click', function () {
-		tabButton.removeClass('orange');
-		tabButton.eq(i).addClass('orange');
-		tabContent.removeClass('show');
-		tabContent.eq(i).addClass('show');
-	});
+// for (let i = 0; i < tabNum; i++) {
+// 	tabButton.eq(i).on('click', function () {
+// 		openTab(i);
+// 	});
+// }
+function openTab(num) {
+	tabButton.removeClass('orange');
+	tabButton.eq(num).addClass('orange');
+	tabContent.removeClass('show');
+	tabContent.eq(num).addClass('show');
 }
+$('.list').click(function (e) {
+	openTab(Number(e.target.dataset.id));
+});
